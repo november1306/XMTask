7cba4954079a9fdde87830070a78905e2f3bafe4
dmytro.hrynevych <dmytro.hrynevych@hermes-europe.co.uk>
2022-06-09 17:35:59 +0300

    maven template with dependencies. stub test

---
+++ src/test/java/DateTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class DateTest {
+    Logger log = LoggerFactory.getLogger(DateTest.class);
+
+    @Test
+    void checkDateTest() {
+        log.info("template for date check test");
+
+        Assertions.assertTrue(true);
+    }
+}
